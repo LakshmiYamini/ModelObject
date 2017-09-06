ddf8f3f63f107eefc5502d767e0525685155c754
iMicron_IOSDev <imicron_iosdev@iMicron-IOSDevs-Mac-mini.local>
2017-09-06 15:01:19 +0530

    Initial Commit

---
+++ ModelObject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModelObject
+//
+//  Created by iMicron_IOSDev on 9/6/17.
+//  Copyright © 2017 iMicron_IOSDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
